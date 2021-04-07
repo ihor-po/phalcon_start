71ff78a2025671e4ba649da135cad408c251d36f
Igor Porotikov <sobikashi@gmail.com>
2021-04-07 09:18:50 +0300

    fixed signout url

--- app/views/templates/dashboard.volt%%e%%.php
+++ app/views/templates/dashboard.volt%%e%%.php
@@ -1,36 +1,36 @@
-<!DOCTYPE html>
+a:5:{i:0;s:266:"<!DOCTYPE html>
 <html lang="en">
 <head>
-    {{ getTitle() }}
+    <?= $this->tag->gettitle() ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{ this.assets.outputCss('style') }}
-    {{ this.assets.outputJs('js') }}
-    {% block head %}
+    <?= $this->assets->outputCss('style') ?>
+    <?= $this->assets->outputJs('js') ?>
+    ";s:4:"head";a:1:{i:0;a:4:{s:4:"type";i:357;s:5:"value";s:6:"
 
-    {% endblock %}
+    ";s:4:"file";s:37:"../app/views/templates/dashboard.volt";s:4:"line";i:11;}}i:1;s:1424:"
 </head>
 <body>
     <div class="navbar navbar-default">
         <div class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-light bg-light w-100">
-          <a class="navbar-brand" href="{{ url('dashboard/') }}">Fireball</a>
+          <a class="navbar-brand" href="<?= $this->url->get('dashboard/') ?>">Fireball</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item active">
-                <a class="nav-link" href="{{ url('dashboard/') }}">Dashboard <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="<?= $this->url->get('dashboard/') ?>">Dashboard <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="{{ url('project/') }}">Projects</a>
+                <a class="nav-link" href="<?= $this->url->get('project/') ?>">Projects</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="{{ url('account/') }}">Account</a>
+                <a class="nav-link" href="<?= $this->url->get('account/') ?>">Account</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="{{url('/index/signout/') }}">Signout</a>
+                <a class="nav-link" href="<?= $this->url->get('/index/signout/') ?>">Signout</a>
               </li>
             </ul>
           </div>
@@ -39,13 +39,13 @@
     </div>
 
     <div class="container">
-    {{ flash.output() }}
+    <?= $this->flash->output() ?>
 
         <div>
-            {% block content %}
+            ";s:7:"content";a:1:{i:0;a:4:{s:4:"type";i:357;s:5:"value";s:14:"
 
-            {% endblock %}
+            ";s:4:"file";s:37:"../app/views/templates/dashboard.volt";s:4:"line";i:47;}}i:2;s:42:"
         </div>
     </div>
 </body>
-</html>
+</html>";}
